31d6716c189f5679426bb225ca78d97087397c98
malik <99975394+malikbendevv@users.noreply.github.com>
2022-10-16 00:52:53 +0100

    Update fetchFromApi.js

--- src/utils/fetchFromApi.js
+++ src/utils/fetchFromApi.js
@@ -6,7 +6,7 @@ const options = {
     maxResults: "50",
   },
   headers: {
-    "X-RapidAPI-Key": "34dff9d00amsh59e4df28d5514b1p1f9345jsn5ccb05ce0650",
+    "X-RapidAPI-Key": '34dff9d00amsh59e4df28d5514b1p1f9345jsn5ccb05ce0650',
     "X-RapidAPI-Host": "youtube-v31.p.rapidapi.com",
   },
 };
